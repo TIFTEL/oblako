6362dbcb8c34b564ade02f11e82d9029b531e552
Tarasov_Georgiy <99883221+TIFTEL@users.noreply.github.com>
2023-05-03 00:41:17 +0700

    +login

--- server/config/default.js
+++ server/config/default.js
@@ -1,5 +1,6 @@
 const data = {
     serverPort: 5000,
-    dbURL: "mongodb+srv://tysachnyk1111:5TgM4aVoo@cluster0.czlbpfo.mongodb.net/?retryWrites=true&w=majority"
+    dbURL: "mongodb+srv://tysachnyk1111:5TgM4aVoo@cluster0.czlbpfo.mongodb.net/?retryWrites=true&w=majority",
+    secretKey: "Secret-key"
 }
 module.exports= {data}
--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,6 +1,7 @@
 const Router=require('express');
 const User = require("../models/User")
 const bcrypt = require("bcryptjs")
+const {data} = require("../config/default.js")
 const jwt = require("jsonwebtoken")
 const {check, validationResult}=require("express-validator")
 const router = new Router()
@@ -18,11 +19,11 @@ router.post('/registration',
         }
         const {email,password}=req.body
 
-        const candidate = User.findOne({email})
+        const candidate = await User.findOne({email})
         if(candidate){
             return res.status(400).json({message:`User with email ${email} already exist`})
         }
-        const hashPassword=await bcrypt.hash(password,15)
+        const hashPassword=await bcrypt.hash(password,8)
         const user = new User({email,password:hashPassword})
         await user.save()
         return res.json({message:"User was created"})
@@ -32,4 +33,36 @@ router.post('/registration',
     }
 })
 
+
+router.post('/login',
+    async (req,res)=>{
+        try{
+            const [email, password]=req.body
+            const user =await  User.findOne({email})
+
+            if(!user){
+                return res.status(404).json({message:"User not found"})
+            }
+            const isPassValid = bcrypt.compareSync(password, user.password)
+
+            if(!isPassValid){
+                return res.status(400).json({message:"invalid password"})
+            }
+
+            const token=jwt.sign({id:user.id}, data.secretKey, {expiresIn: "1h"})
+            return res.json({
+                token,
+                user: {
+                    id: user.id,
+                    email: user.email,
+                    diskSpace: user.diskSpace,
+                    usedSpace: user.usedSpace,
+                    avatar: user.avatar
+                }
+            })
+        }catch (e){
+            console.log(e)
+            res.send({message:"Server error"})
+        }
+    })
 module.exports=router
